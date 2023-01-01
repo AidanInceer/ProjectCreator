384729e1d36c4133f72564c3c56a6dcffb3072e9
AidanInceer <aidaninceer0@gmail.com>
2023-01-01 21:34:42 +0000

    remove obsolete imports

--- projectcreator/core/generate.py
+++ projectcreator/core/generate.py
@@ -1,5 +1,4 @@
 import os
-from argparse import ArgumentParser
 from dataclasses import dataclass
 
 from projectcreator.utils.config import Config
@@ -16,7 +15,7 @@ class Generate:
             for k, v in directory.items():
                 self.create_file_or_folder(path, k)
                 current_depth_path = path
-                path = path + k + r"\\"
+                path = path + k + r'\\'
                 if is_dict(v):
                     self.create_directory(v, path)
                 elif is_list(v):
@@ -32,11 +31,11 @@ class Generate:
             self.create_file_or_folder(path, directory)
 
     def create_file_or_folder(self, dir_path: str, object: str | list | dict) -> None:
-        folders = self.config["file_to_folders"]
-        files = self.config["folder_to_files"]
+        folders = self.config['file_to_folders']
+        files = self.config['folder_to_files']
 
         # Does the path exists allready?
-        if os.path.exists(f"{dir_path}{object}"):
+        if os.path.exists(f'{dir_path}{object}'):
             pass
 
         # Does the object follow dot notation file rules
@@ -47,7 +46,7 @@ class Generate:
 
         # Default file/folder check
         else:
-            if self.is_file(object) == "None":
+            if self.is_file(object) == 'None':
                 pass
             if self.is_file(object):
                 self.create_file(dir_path, object)
@@ -57,36 +56,36 @@ class Generate:
     @staticmethod
     def is_file(object: str | list | dict | None) -> bool:
         if object is None:
-            return "None"
-        elif "." in object:
+            return 'None'
+        elif '.' in object:
             return True
         else:
             return False
 
     @staticmethod
     def create_folder(path: str, folder_name: str) -> str:
-        logger.debug(f"creating folder: {path}{folder_name}")
+        logger.debug(f'creating folder: {path}{folder_name}')
         folder_path = path + folder_name
         os.mkdir(folder_path)
 
     def create_file(self, path: str, file_name: str) -> str:
-        logger.debug(f"creating file: {path}{file_name}")
+        logger.debug(f'creating file: {path}{file_name}')
         if file_name is not None:
-            boilerplate_files = self.config["boilerplate_files"]
+            boilerplate_files = self.config['boilerplate_files']
             file_path = path + file_name
             if file_name in boilerplate_files:
-                with open(f"./data/{file_name}", "r") as r_file:
+                with open(f'./data/{file_name}', 'r') as r_file:
                     data = r_file.readlines()
-                with open(file_path, "w") as w_file:
+                with open(file_path, 'w') as w_file:
                     w_file.writelines(data)
             else:
-                with open(file_path, "x") as _:
+                with open(file_path, 'x') as _:
                     pass
         else:
             pass
 
     @staticmethod
     def root_folder(project_path: str, project_name: str) -> None:
-        path = project_path + "\\" + project_name + "\\"
-        os.mkdir(f"{path}")
+        path = project_path + '\\' + project_name + '\\'
+        os.mkdir(f'{path}')
         return path
